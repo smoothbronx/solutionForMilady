b6b8fdbe3439baa28c3664ef7f44cb03fb183b48
smoothbronx <vlad.uhwatov@yandex.ru>
2023-03-15 03:03:27 +0200

    Lab 5

---
+++ ConsoleApp6/LessZeroException.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp6;
+
+public class LessZeroException : Exception
+{
+    public LessZeroException(string name) : base($"{name} cannot be less than zero.")
+    {
+    }
+
+    public LessZeroException() : base("Value cannot be less than zero.")
+    {
+    }
+
+    public LessZeroException(string name, double value) : base($"{name} cannot be less than zero. <{name}: {value:F2}>")
+    {
+    }
+}
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,38 +1,51 @@
 ﻿using ConsoleApp6;
 
-var aeroflots = new Aeroflot<int>[4];
-for (var index = 0; index < aeroflots.Length; index++)
+double ConvertTime(double hours, double minutes)
 {
-    Console.Write("Введите номер рейса: ");
-    var flightNumber = int.Parse(Console.ReadLine()!);
+    return (hours * 60 + minutes) * 60;
+}
+
+double ConvertTimeOrThrowException(double hours, double minutes)
+{
+    if (double.IsNegative(hours)) throw new Exception("Hours cannot be less than zero");
+    if (double.IsNegative(minutes)) throw new Exception("Minutes cannot be less than zero");
+
+    return ConvertTime(hours, minutes);
+}
+
+double ConvertTimeOrThrowArgumentException(double hours, double minutes)
+{
+    if (double.IsNegative(hours)) throw new ArgumentException("Hours cannot be less than zero");
+    if (double.IsNegative(minutes)) throw new ArgumentException("Minutes cannot be less than zero");
     
-    Console.Write("Введите пункт назначения: ");
-    var destination = Console.ReadLine()!;
+    return ConvertTime(hours, minutes);
+}
+
+double ConvertTimeOrThrowSpecificException(double hours, double minutes)
+{
+    if (double.IsNegative(hours)) throw new LessZeroException("Hours", hours);
+    if (double.IsNegative(minutes)) throw new LessZeroException("Minutes", minutes);
     
-    Console.Write("Введите тип самолета: ");
-    var aircraftType = Console.ReadLine()!;
-    Console.WriteLine();
-
-    aeroflots[index] = new Aeroflot<int>(flightNumber, destination, aircraftType);
+    return ConvertTime(hours, minutes);
 }
 
-var aeroflotsList = aeroflots.ToList();
-aeroflotsList.Sort((x, y) => y.CompareTo(x));
-
-Console.WriteLine("\nСписок введенных объектов:");
-foreach (var aeroflot in aeroflotsList)
-    aeroflot.Display();
-
-Console.Write("\nВведите интересующий пункт назначения: ");
-var targetDestination = Console.ReadLine()!;
-
-var searchResult = aeroflotsList.FindAll((item) => item.Destination == targetDestination);
-if (searchResult.Count != 0)
+void Main()
 {
-    foreach (var aeroflot in searchResult)
-        aeroflot.Display();
+    Console.Write("Введите значение часов: ");
+    var hours = double.Parse(Console.ReadLine()!);
+    
+    Console.Write("Введите значение минут: ");
+    var minutes = double.Parse(Console.ReadLine()!);
+
+    try
+    {
+        var result = ConvertTimeOrThrowSpecificException(hours, minutes);
+        Console.WriteLine($"Результат преобразования: {result:F2}");
+    }
+    catch (LessZeroException exception)
+    {
+        Console.WriteLine(exception.Message);
+        throw;
+    }
 }
-else
-{
-    Console.WriteLine("Самолеты, идущие в интересующий пункт назначения, отсутствуют");
-}
+Main();
