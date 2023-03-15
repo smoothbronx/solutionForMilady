d87b834262f26ec446817454d7e6dc70bf792fa7
smoothbronx <vlad.uhwatov@yandex.ru>
2023-03-15 02:37:44 +0200

    Lab 4

---
+++ ConsoleApp6/Aeroflot.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp6;
+
+public class Aeroflot<T> : ICloneable, IComparable
+{
+    public T FlightNumber { get; set; }
+
+    public string Destination { get; set; }
+
+    public string AircraftType { get; set; }
+
+    public Aeroflot(T flightNumber, string destination, string aircraftType)
+    {
+        FlightNumber = flightNumber;
+        Destination = destination;
+        AircraftType = aircraftType;
+    }
+
+    public object Clone()
+    {
+        return new Aeroflot<T>(FlightNumber, Destination, AircraftType);
+    }
+
+    public int CompareTo(Aeroflot<string> other)
+    {
+        if (typeof(T) == typeof(string))
+            return string.CompareOrdinal(Convert.ToString(FlightNumber), other.FlightNumber);
+        return 1;
+    }
+    
+    public int CompareTo(Aeroflot<int> other)
+    {
+        if (typeof(T) == typeof(string))
+            return -1;
+        return other.FlightNumber - Convert.ToInt32(FlightNumber);
+    }
+    
+    public int CompareTo(object? obj)
+    {
+        if (obj == null) return 1;
+        throw new ArgumentException("Object is not a Aeroflot");
+    }
+
+    public override string ToString()
+    {
+        return $"<Aeroflot> [{FlightNumber}, {Destination}, {AircraftType}]";
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(ToString());
+    }
+}
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,41 +1,38 @@
 ﻿using ConsoleApp6;
 
-// Равносторонний треугольник
-var equilateral = new Triangle(3, 3, 3, 60, 60, 60);
-Console.WriteLine("Sides\tAngles\tHeights\n" +
-                  $"a = {equilateral.ASide}\tA = {equilateral.AAngle}\tH(a) = {equilateral.AHeight():F2}\n" +
-                  $"b = {equilateral.BSide}\tB = {equilateral.BAngle}\tH(b) = {equilateral.BHeight():F2}\n" +
-                  $"c = {equilateral.CSide}\tC = {equilateral.CAngle}\tH(c) = {equilateral.CHeight():F2}\n");
-Console.WriteLine($"Тип треугольника: {equilateral.Type()}");
-Console.WriteLine($"Периметр: {equilateral.Perimeter():F2}");
-Console.WriteLine($"Площадь: {equilateral.Area():F2}\n\n");
+var aeroflots = new Aeroflot<int>[4];
+for (var index = 0; index < aeroflots.Length; index++)
+{
+    Console.Write("Введите номер рейса: ");
+    var flightNumber = int.Parse(Console.ReadLine()!);
+    
+    Console.Write("Введите пункт назначения: ");
+    var destination = Console.ReadLine()!;
+    
+    Console.Write("Введите тип самолета: ");
+    var aircraftType = Console.ReadLine()!;
+    Console.WriteLine();
 
-// Прямоугольный треугольник
-var straight = new Triangle(3, 4, 5, 45, 45, 90);
-Console.WriteLine("Sides\tAngles\tHeights\n" +
-                  $"a = {straight.ASide}\tA = {straight.AAngle}\tH(a) = {straight.AHeight():F2}\n" +
-                  $"b = {straight.BSide}\tB = {straight.BAngle}\tH(b) = {straight.BHeight():F2}\n" +
-                  $"c = {straight.CSide}\tC = {straight.CAngle}\tH(c) = {straight.CHeight():F2}\n");
-Console.WriteLine($"Тип треугольника: {straight.Type()}");
-Console.WriteLine($"Периметр: {straight.Perimeter():F2}");
-Console.WriteLine($"Площадь: {straight.Area():F2}\n\n");
+    aeroflots[index] = new Aeroflot<int>(flightNumber, destination, aircraftType);
+}
 
-// Равнобедренный треугольник
-var isosceles = new Triangle(4, 4, 2, 70, 70, 40);
-Console.WriteLine("Sides\tAngles\tHeights\n" +
-                  $"a = {isosceles.ASide}\tA = {isosceles.AAngle}\tH(a) = {isosceles.AHeight():F2}\n" +
-                  $"b = {isosceles.BSide}\tB = {isosceles.BAngle}\tH(b) = {isosceles.BHeight():F2}\n" +
-                  $"c = {isosceles.CSide}\tC = {isosceles.CAngle}\tH(c) = {isosceles.CHeight():F2}\n");
-Console.WriteLine($"Тип треугольника: {isosceles.Type()}");
-Console.WriteLine($"Периметр: {isosceles.Perimeter():F2}");
-Console.WriteLine($"Площадь: {isosceles.Area():F2}");
+var aeroflotsList = aeroflots.ToList();
+aeroflotsList.Sort((x, y) => y.CompareTo(x));
 
-// Разносторонний треугольник
-var versatile = new Triangle(3, 4, 2, 70, 20, 90);
-Console.WriteLine("Sides\tAngles\tHeights\n" +
-                  $"a = {versatile.ASide}\tA = {versatile.AAngle}\tH(a) = {versatile.AHeight():F2}\n" +
-                  $"b = {versatile.BSide}\tB = {versatile.BAngle}\tH(b) = {versatile.BHeight():F2}\n" +
-                  $"c = {versatile.CSide}\tC = {versatile.CAngle}\tH(c) = {versatile.CHeight():F2}\n");
-Console.WriteLine($"Тип треугольника: {versatile.Type()}");
-Console.WriteLine($"Периметр: {versatile.Perimeter():F2}");
-Console.WriteLine($"Площадь: {versatile.Area():F2}");
+Console.WriteLine("\nСписок введенных объектов:");
+foreach (var aeroflot in aeroflotsList)
+    aeroflot.Display();
+
+Console.Write("\nВведите интересующий пункт назначения: ");
+var targetDestination = Console.ReadLine()!;
+
+var searchResult = aeroflotsList.FindAll((item) => item.Destination == targetDestination);
+if (searchResult.Count != 0)
+{
+    foreach (var aeroflot in searchResult)
+        aeroflot.Display();
+}
+else
+{
+    Console.WriteLine("Самолеты, идущие в интересующий пункт назначения, отсутствуют");
+}
